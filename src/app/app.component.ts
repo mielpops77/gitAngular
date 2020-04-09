39bcbec15eda439c6705e970e0e98c7eb4c467e0
mielpops <zeitounamiel@MacBook-Pro-de-zeitoun.local>
2020-04-09 20:40:29 +0200

    2 premier chap rxjx terminé

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import {AppareilService} from './service/appareil.service';
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+import 'rxjs/add/observable/interval';
+
 
 @Component({
   selector: 'app-root',
@@ -8,5 +11,34 @@ import {AppareilService} from './service/appareil.service';
 })
 export class AppComponent {
   title = 'angularGit';
+  secondes : number;
+  counterSubscription: Subscription;
+
+
+
+  ngOnInit() {
+    const counter = Observable.interval(1000);
+
+    this.counterSubscription =counter.subscribe(
+      (value) => {
+        this.secondes = value;
+      },
+      (error) =>
+      {
+        console.log("Uh-oh, an error occurred !:"+error);
+      },
+      ()=>
+      {
+        console.log("observable complete !");
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.counterSubscription.unsubscribe();
+  }
+
+
+
   
 }
