5964c93a329e169555891438020e49d47746db3b
mielpops <zeitounamiel@MacBook-Pro-de-zeitoun.local>
2020-04-09 12:26:48 +0200

    couleurs + date

---
+++ src/app/appareil/appareil.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit, Input } from '@angular/core';
+import {appareilService} from '../service/appareil.service';
+@Component({
+  selector: 'app-appareil',
+  templateUrl: './appareil.component.html',
+  styleUrls: ['./appareil.component.css']
+})
+export class AppareilComponent implements OnInit {
+
+ 
+
+@Input() appareilName : string;
+@Input() appareilStatus : string;
+
+
+constructor( private appareilservice : appareilService) 
+{
+}
+  ngOnInit(): void {
+   
+  }
+  getColor()
+  {
+    if (this.appareilStatus === 'allumé')
+    {
+      return 'green'
+
+    }
+
+    else if(this.appareilStatus === 'éteint') {
+    
+      return 'red'
+      
+    }
+  }
+
+  lastUpdate = new Promise((resolve, reject) => {
+    const date = new Date();
+    setTimeout(
+      () => {
+        resolve(date);
+      }, 2000
+    );
+  });
+
+}
---
+++ src/app/service/appareil.service.ts
@@ -0,0 +1,22 @@
+export class appareilService
+{
+    appareils=
+    [
+        {
+            name:"Machine à laver",
+            status :"éteint"
+        },
+        {
+            name:"Frigo",
+            status :"allumé"
+
+        },
+        {
+            name:"Ordinateur",
+            status :"éteint"
+
+        }
+
+    ]
+    
+}
