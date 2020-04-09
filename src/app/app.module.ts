e6f1acd26a4f50d0cee8adfc4575ec6d5adae36f
mielpops <zeitounamiel@MacBook-Pro-de-zeitoun.local>
2020-04-09 16:36:39 +0200

    redirection Not found

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,13 +20,16 @@ import { AppareilViewComponent } from './appareil-view/appareil-view.component';
 
 import {Routes,RouterModule} from '@angular/router';
 import { SingleAppareilComponent } from './single-appareil/single-appareil.component';
+import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
 
 const appRoutes : Routes = 
 [
   {path: 'appareils', component: AppareilViewComponent},
   { path: 'appareils/:id', component: SingleAppareilComponent },
   {path: 'auth', component: AuthComponent},
-  {path: '', component: AppareilViewComponent}
+  {path: '', component: AppareilViewComponent},
+  { path: 'not-found', component: FourOhFourComponent },
+  { path: '**', redirectTo: 'not-found' }
 ]
 
 @NgModule({
@@ -35,7 +38,8 @@ const appRoutes : Routes =
     AppareilComponent,
     AuthComponent,
     AppareilViewComponent,
-    SingleAppareilComponent
+    SingleAppareilComponent,
+    FourOhFourComponent
   ],
   imports: [
     BrowserModule,
